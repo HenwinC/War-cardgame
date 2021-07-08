181a24bf0318601737d92d8200cc8163d92d50d3
HenwinC <Henwincuff@gmail.com>
2021-07-08 03:46:31 -0500

    before render function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ let deckEl = document.getElementById('deck')
 let playerdeckEl = document.getElementById('playerDeck')
 let card1El = document.getElementById('card1')
 let card2El = document.getElementById('card2')
+let player1Count = document.getElementById('player1Count')
+let player2Count =document.getElementById('player2Count')
 
 document.getElementById('btn').addEventListener('click', shuffle)
 
@@ -26,7 +28,9 @@ let card2 = playerDeck.splice(0,1)
 const CARD_VALUE_MAP = {
   dA: 14 ,dQ: 12,dK: 13,dJ: 11,d10:10,d09: 9,d08: 8,d07: 7,d06: 6,d05: 5,d04: 4,d03: 3,d02: 2,hA: 14,hQ: 12,hK: 13,hJ: 11,h10:10,h09: 9,h08: 8,h07: 7,h06: 6,h05: 5,h04: 4,h03: 3,h02: 2,cA: 14,cQ: 12,cK: 13,cJ: 11,c10:10,c09: 9,c08: 8,c07: 7,c06: 6,c05: 5,c04: 4,c03: 3,c02: 2,sA: 14,sQ: 12,sK: 13,sJ: 11,s10:10,s09: 9,s08: 8,s07: 7,s06: 6,s05: 5,s04: 4,s03: 3,s02: 2
 }
-
+function render(params) {
+  
+}
 // card1.push(deck.splice(0,1))
 function round(card1, card2){
 
@@ -40,7 +44,7 @@ if (CARD_VALUE_MAP[card2] > CARD_VALUE_MAP[card1]){
  } 
  if (CARD_VALUE_MAP[card2] = CARD_VALUE_MAP[card1]){
   War()}
-  
+
 }
  function war(){
    if (playerDeck.length<4){
@@ -86,6 +90,17 @@ function checkWin() {
  }
  function PlayerLose() {
    console.log('L')
+ }
+ function cleanBeforeRound(){
+   playerdeckEl.innerHTML = ''
+   deckEl.innerHTML=''
+   card1El.innerHTML= ''
+   card2El.innerHTML =''
+   updateDeckCount ()
+ }
+ function updatedDeckCount() {
+
+   
  }
 // card1.push(deck.splice(0,1))
 // render (card1El)
