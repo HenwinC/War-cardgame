7e0c1e09dc262b6593c208652365fcabbc187eed
HenwinC <Henwincuff@gmail.com>
2021-07-09 11:14:09 -0500

    render

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ To play this game please click on the link in the features section. I chose to c
 
 This is how the game works:
 War is a card game for two players. A standard deck of 52 cards is dealt so that both players have 26 cards. During each round of battle, both players play a card from the top of their hand face up. The player who plays the card of the higher value wins both cards and places them at the bottom of their stack of cards. If both cards played are of the same value, then War is initiated --both players play three additional cards face down and then one more card face up. The player who wins the war by playing the higher card wins all eight cards. If the value are still the same, additional wars are played until one player wins the turn. If either player runs out of cards during war to play, they lose the game. The player that accumulates all 52 cards leaving an opponent with no cards is the winner.
------Picture
----picture
+￼
+
 Features
 Language used JavaScript, HTML, CSS,BootStrap
 Hosted on GitHub at https://henwinc.github.io/War-cardgame/
--- js/app.js
+++ js/app.js
@@ -6,15 +6,14 @@ let player1Count = document.getElementById('player1Count')
 let player2Count = document.getElementById('player2Count')
 let text =document.getElementById('')
 let deck=[]
-let end = false
 document.getElementById('btn').addEventListener("click", render)
-// document.getElementById('btn').addEventListener("click", render())
 const CARD_VALUE_MAP = {
   dA: 14 ,dQ: 12,dK: 13,dJ: 11,d10:10,d09: 9,d08: 8,d07: 7,d06: 6,d05: 5,d04: 4,d03: 3,d02: 2,hA: 14,hQ: 12,hK: 13,hJ: 11,h10:10,h09: 9,h08: 8,h07: 7,h06: 6,h05: 5,h04: 4,h03: 3,h02: 2,cA: 14,cQ: 12,cK: 13,cJ: 11,c10:10,c09: 9,c08: 8,c07: 7,c06: 6,c05: 5,c04: 4,c03: 3,c02: 2,sA: 14,sQ: 12,sK: 13,sJ: 11,s10:10,s09: 9,s08: 8,s07: 7,s06: 6,s05: 5,s04: 4,s03: 3,s02: 2
 }
 init()
+// Create deck shuffle and make DeckArrays
 function init() {
-  const suits = ['h', 'd', 's', 'c']
+const suits = ['h', 'd', 's', 'c']
 const values = ['02','03','04','05','06','07','08','09','10','J','Q','K','A']
   
 for (let suitCounter = 0; suitCounter<4; suitCounter++){
@@ -22,18 +21,18 @@ for (let suitCounter = 0; suitCounter<4; suitCounter++){
     deck.push(suits[suitCounter]+values[i])
   }
 }
+}
 //  Randomize Deck
 function shuffle (){
 deck.sort((a,b)=> 0.5 - Math.random())
+// splitdeck in half and push first elements in claying card array
+
 }
-shuffle(deck)
-}
-// splits deck in half and pushes to new array
-let playerDeck = deck.splice(0,26)
-let card1 = deck.slice(0,1)
-let card2 = playerDeck.slice(0,1)
+  shuffle(deck)
+  let playerDeck = deck.splice(0,26)
+  const card1 = deck.slice(0,1)
+  const card2 = playerDeck.slice(0,1)
 function render() {
-  
   if(card1===1){
     card1El.classList.remove(cartToRemove)
   }
@@ -46,25 +45,23 @@ function render() {
   if(card2===1){
     card2El.classList.remove(cartToRemove1)
   }
-  cardToRemove1 = card2
-  card2El.classList.add(card2)
-  card2El.classList.add('animate__animated','animate__rotateOutUpRight')
-  setTimeout(()=> {
-    card2El.classList.remove('animate__animated','animate__rotateOutUpRight')
+     cardToRemove1 = card2
+     card2El.classList.add(card2)
+     card2El.classList.add('animate__animated',    'animate__rotateOutUpRight')
+     setTimeout(()=> {
+     card2El.classList.remove('animate__animated','animate__rotateOutUpRight')
   }, 1000)
-  round()
+round()
 }
 
-// document.getElementById('btn').addEventListener("click", render)
-// render()
 
 // this function will determine who round winner is
 // if card value is greater than other push the card into others array. while also taking off top card
 function round(){
-  if (deck.length === 0){
+  if(deck.length && card1.length=== 0){
     player1Win()
   }
-  if (playerDeck.length === 0){
+  if(playerDeck.length && card2.length=== 0){
     Player2Win()
   }
 
@@ -73,28 +70,27 @@ function round(){
     playerDeck.push(card1[0])
     playerDeck.shift(card2[0])
     playerDeck.push (card2[0])
-    console.log('yur')
-    console.log ('yessir')
+    checkWin()
   }
    if (CARD_VALUE_MAP[card2[0]] < CARD_VALUE_MAP[card1[0]]){
     playerDeck.shift(card2[0])
     deck.push(card2[0])
     deck.shift(card1[0])
     deck.push(card1[0])
-    console.log('joe')
+    checkWin()
    } 
    if (CARD_VALUE_MAP[card2] === CARD_VALUE_MAP[card1]){
      war()
   }  
+   checkWin()
 }
-
 // if values are equal each player will place 3 cards into a war deck and flip a forth to see who wins all 8 cards
  function war(){
    if (playerDeck.length < 4){
-     Player2Win()
+     playerLose()
    }
    if (deck.length < 4){
-     player1Win()
+     playerWin()
    }
    let warCard = deck.shift(3,1)
    let playerWarCard = playerDeck.shift(3,1)
@@ -106,17 +102,17 @@ function round(){
    war6 = playerDeck.shift(0,1)
 
   if (CARD_VALUE_MAP[playerWarCard[0]] > CARD_VALUE_MAP[warCard[0]]){
-      deck.shift(card1[0])
-      playerDeck.shift(card2[0])
-      playerDeck.push(card1[0])
-      playerDeck.push(card2[0])
-      playerDeck.push(war1[0])
-      playerDeck.push(war2[0])
-      playerDeck.push(war3[0])
-      deck.push(warCard[0])
-      deck.push(playerWarCard[0])
-      }
-  if (CARD_VALUE_MAP[playerWarCard[0]]< CARD_VALUE_MAP[warCard[0]]) {
+    deck.shift(card1[0])
+    playerDeck.shift(card2[0])
+    playerDeck.push(card1[0])
+    playerDeck.push(card2[0])
+    playerDeck.push(war1[0])
+    playerDeck.push(war2[0])
+    playerDeck.push(war3[0])
+    deck.push(warCard[0])
+    deck.push(playerWarCard[0])
+  }
+  if (CARD_VALUE_MAP[playerWarCard[0]]< CARD_VALUE_MAP[warCard[0]]){
     playerDeck.shift(card2[0])
     deck.shift(card1[0])
     deck.push(card2[0])
@@ -126,75 +122,29 @@ function round(){
     deck.push(war6[0])
     deck.push(warCard[0])
     deck.push(playerWarCard[0])
-    }
+  }
   if (CARD_VALUE_MAP[playerWarCard[0]] = CARD_VALUE_MAP[warCard[0]]){
     war()
-    }
+  }
+  checkWin ()
  }
 //  function that checks win or starts new round
-// function checkWin() {
-//   console.log ('winf')
-//   if (end = !false){
-//     console.log('done')
-//   }
-//   if (deck.length===0){
-//     player1Win() 
-
-//     end = !false}
-
-//   if (playerDeck.length===0){
-//     Player2Win() 
-
-//     end = !false} 
-  
-//   if (end = false) {
-//     render()
-//     }
-// }
+function checkWin() {
+  console.log ('winf')
+  if (deck.length===0){
+    player1Win() 
+  }
+  if (playerDeck.length===0){
+    Player2Win() } 
+  else render()
+}
 // display text is player wins
  function player1Win(){
-
-  console.log('Dub')
+  text.innerText('Player 1 Wins!')
 
  }
 //  display text is player loses
  function Player2Win() {
-   console.log('player2')
+   text.innerText('Player 2 Wins!')
  }
-//  document.getElementById('btn').addEventListener("click", render)
-//  show card count
-//  function updatedDeckCount() {
-//   let player2Count= (deck.length)
-//   playerDeck.length = player1Count.innerText('player1Count')
-//    getNewPlayerCards ()
-//  }
-// updatedDeckCount()
-// document.getElementById('btn').addEventListener("click", getNewCard())
-
-
-// First get a deck of card
-// Deck of cards includes 4 suits(Hearts,Spades, Dimaonds, Clubs) 0f cards 2-J,Q,K,A
-// Next Shuffle Deck
-// shuffle deck
-// split deck into two
-// each player will flip the first card 
-// if player1Card > player2Card
-// add playercards to player1Deck
-// if player2Card < player1Card
-// add playercards to player2Deck
-// if player1Card === Player2Card
-// draw three cards each or if deck.length =<4 end game
-// if player1Card > player2Card
-// add all 8 playercards to player1Deck
-// if player1Card < player2Card
-// add all 8 playercards to player2Deck
-// if player1Deck === 0
-// player 2 wins
-// if player2Deck === 0
-// player 1 wins
-// const playerCard = playerDeck.pop()
-//   const computerCard = computerDeck.pop()
-
-//   playerCardSlot.appendChild(playerCard.getHTML())
-//   computerCardSlot.appendChild(computerCard.getHTML())
-  
+ 
