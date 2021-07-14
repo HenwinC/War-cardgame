3e76b17f7bd31d7ed5a344009806870f2806af65
HenwinC <Henwincuff@gmail.com>
2021-07-14 16:32:11 -0500

    Final

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ let score2 = document.getElementById('score2')
 let deck=[]
 document.getElementById('btn').addEventListener("click", game)
 const CARD_VALUE_MAP = {
-  dA: 14 ,dQ: 12,dK: 13,dJ: 11,d10:10,d09: 9,d08: 8,d07: 7,d06: 6,d05: 5,d04: 4,d03: 3,d02: 2,hA: 14,hQ: 12,hK: 13,hJ: 11,h10:10,h09: 9,h08: 8,h07: 7,h06: 6,h05: 5,h04: 4,h03: 3,h02: 2,cA: 14,cQ: 12,cK: 13,cJ: 11,c10:10,c09: 9,c08: 8,c07: 7,c06: 6,c05: 5,c04: 4,c03: 3,c02: 2,sA: 14,sQ: 12,sK: 13,sJ: 11,s10:10,s09: 9,s08: 8,s07: 7,s06: 6,s05: 5,s04: 4,s03: 3,s02: 2
+  dA: 14 ,dQ: 12,dK: 13,dJ: 11,d10:10,d09: 9,d08: 8,d07: 7,d06: 6,d05: 5,d04: 4,d03: 3,d02: 2,hA: 14,hQ: 12,hK: 13,hJ: 11,h10: 10,h09: 9,h08: 8,h07: 7,h06: 6,h05: 5,h04: 4,h03: 3,h02: 2,cA: 14,cQ: 12,cK: 13,cJ: 11,c10: 10,c09: 9,c08: 8,c07: 7,c06: 6,c05: 5,c04: 4,c03: 3,c02: 2,sA: 14,sQ: 12, sK: 13,sJ: 11,s10:10,s09: 9,s08: 8,s07: 7,s06: 6,s05: 5,s04: 4,s03: 3,s02: 2
 }
 init()
 // Create deck shuffle and make DeckArrays
@@ -37,13 +37,13 @@ function shuffle (){
 function render() {
 let card1 = deck[0]
 let card2 = playerDeck[0]
+let card3 = document.querySelector('#card1').classList[document.querySelector('#card1').classList.length -1]
+let card4 = document.querySelector('#card2').classList[document.querySelector('#card2').classList.length -1]
 
-  if (card1===!0){
-    card1El.classList.remove(card1)
-    }
+  card1El.classList.remove(card3)
 
-  cardToRemove = card1
   card1El.classList.add(card1)
+  
   card1El.classList.add('animate__animated','animate__rotateOutUpLeft')
 
   setTimeout(()=> {
@@ -51,10 +51,9 @@ let card2 = playerDeck[0]
     }, 1000)
 
 
-  if(card2===!1){
-    card2El.classList.remove(card2)
-  }
-     cardToRemove1 = card2
+  
+    card2El.classList.remove(card4)
+
      card2El.classList.add(card2)
      card2El.classList.add('animate__animated',    'animate__rotateOutUpRight')
      
@@ -65,15 +64,15 @@ let card2 = playerDeck[0]
 // if card value is greater than other push the card into others array. while also taking off top card
 function round(){
   if (deck.length === 0){
-     player1Win()
+     text.innerText= ('Player 2 Wins!')
+
     }
 
   if (playerDeck.length === 0){
-     Player2Win()
+     Ptext.innerText= ('Player 1 Wins!') 
     }
 
   if (CARD_VALUE_MAP[playerDeck[0]] > CARD_VALUE_MAP[deck[0]]){
-      text.innerText= ('Player 2 wins round!')
       deck.shift(deck[0])
       playerDeck.push(deck[0])
       playerDeck.shift(playerDeck[0])
@@ -83,7 +82,6 @@ function round(){
       checkWin()
       }
   else if (CARD_VALUE_MAP[playerDeck[0]] < CARD_VALUE_MAP[deck[0]]){
-      text.innerText= ('Player 1 wins round!')
       playerDeck.shift(playerDeck[0])
       deck.push(playerDeck[0])
       deck.shift(deck[0])
@@ -158,12 +156,11 @@ function round(){
  }
 //  function that checks win 
 function checkWin() {
-
   if (deck.length === 0){
      text.innerText= ('Player 2 Wins!')
       }
 
-  if (playerDeck.length===0){
+  if (playerDeck.length === 0){
      text.innerText= ('Player 1 Wins!') 
       } 
     }
